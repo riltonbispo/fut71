faf940308484dc870db7b4c3d2cb975d55f30c77
riltonbispo <riltondev@gmail.com>
2024-04-19 16:12:02 -0300

    refactor: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Ranking from './_components/Ranking'
+import { DetailsPlayers } from './_components/DetailsPlayers'
+import { MatchList } from './_components/MatchList'
+import { RankingTable } from './_components/RankingTable'
 
 export default function Home() {
   return (
-    <main>
-      <Ranking />
+    <main className="py-4 px-16 grid gap-12 lg:grid-cols-2 lg:px-32">
+      <RankingTable />
+      <div className="space-y-10">
+        <MatchList />
+        <DetailsPlayers />
+      </div>
     </main>
   )
 }
